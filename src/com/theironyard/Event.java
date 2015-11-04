b2f44bd36dcb1ab3990836e463576debc81d9a8f
macbookair <nj.martin123@gmail.com>
2015-11-04 11:53:54 -0500

    Initial Import

---
+++ src/com/theironyard/Event.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by macbookair on 11/4/15.
+ */
+public class Event {
+    int id;
+    String description;
+    LocalDateTime startDate;
+}
